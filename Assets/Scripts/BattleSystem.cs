5114bbe44101bf3f0ba0d5dfc7f9e5a994343572
Seth Tal <seth.tal255@gmail.com>
2020-10-02 22:41:28 -0700

    Added Timer, Started work on both front end and backend
    - added global game timer for tracking time left in a given battle
    
    - created creature prefab and started work on backend for creature behavior
    
    - created global battle manager, started work on backend for battle manager

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSystem : MonoBehaviour
+{
+    [Header("Battle System Data")]
+    public List<bool> _playerCreatures = new List<bool>(new bool[3]);
+    public List<bool> _enemyCreatures = new List<bool>(new bool[3]);
+
+    [Header("Instance Data")]
+    public static BattleSystem _instance;
+    [SerializeField, ReadOnly] private bool _gameStart = false;
+
+
+    void Awake()
+    {
+        if (!_gameStart)
+        {
+            _instance = this;
+            _gameStart = true;
+        }
+    }
+
+    void Start()
+    {
+        GameTimer._instance.StartTimer();
+    }
+
+    // void Update() {}
+    // void FixedUpdate() {}
+}
---
+++ Assets/Scripts/CreatureClickEvents.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureClickEvents : MonoBehaviour
+{
+    public bool _debug_MousePosition = false;
+    [SerializeField, ReadOnly] public Vector2 _mousePos = Vector2.zero;
+
+    // void Awake() {}
+    // void Start() {}
+
+    void Update()
+    {
+        if (_debug_MousePosition)
+            _mousePos = GetMouseWorldPos();
+    }
+
+    // void FixedUpdate() {}
+
+    // void OnMouseDown() {}
+    // void OnMouseUp() {}
+    // void OnMouseEnter() {}
+    // void OnMouseExit() {}
+
+    void OnMouseDrag()
+    {
+        if (Input.GetMouseButton(0))
+            transform.position = new Vector3(GetMouseWorldPos().x, GetMouseWorldPos().y, 0);
+    }
+
+    // void OnMouseOver() {}
+
+    private Vector2 GetMouseWorldPos()
+    {
+        Vector3 getMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return new Vector2(getMouse.x, getMouse.y);
+    }
+}
